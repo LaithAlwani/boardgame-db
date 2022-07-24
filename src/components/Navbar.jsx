3aa87eeef618486da50b3dcfab8fae49b726690c
laith <laithalwani@gmail.com>
2022-07-23 23:27:11 -0400

    adds storage, firestore, auth from firebase, fixes grid layout in games, changes navbar color, font weight and active class

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,25 @@
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 
 export default function Navbar() {
+  const location = useLocation()
+  const path = location.pathname.split("/")[1]
+  console.log(path)
   return (
     <nav>
       <div className="nav-container">
         <div className="logo"><Link to="/">BGDB</Link> </div>
         <div className="nav-links">
-          <Link to="/games">Games</Link>
-          <Link to="/plays">Plays</Link>
-          <Link to="/signin">Sign In</Link>
+          <Link to="/games">
+            <a className={path === "games"? "active" : ""}>Games</a> 
+            </Link>
+          <Link to="/plays">
+          <a className={path === "plays"? "active" : ""}>Plays</a> 
+            
+          </Link>
+          <Link to="/signin">
+          <a className={path === "signin"? "active" : ""}>Sign in</a> 
+
+          </Link>
           <Link to="#">Sign Out</Link>
           
         </div>
