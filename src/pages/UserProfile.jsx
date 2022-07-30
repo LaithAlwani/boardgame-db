5065634b98dc20bbeaab0f0c59316f639dec2c41
laith <laithalwani@gmail.com>
2022-07-29 21:28:27 -0400

    adds game tags

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,6 +1,5 @@
-import {useState } from "react";
+import { useState } from "react";
 import XMLParser from "react-xml-parser";
-
 
 export default function UserProfile() {
   const [username, setUsername] = useState("");
@@ -9,33 +8,61 @@ export default function UserProfile() {
   const thingURL = `thing?type=boardgame`;
 
   const handleClick = () => {
-    
-      fetch(`https://boardgamegeek.com/xmlapi2/${collectionURL}`, {
-        crossDomain: true,
+    fetch(`https://boardgamegeek.com/xmlapi2/${collectionURL}`, {
+      crossDomain: true,
+    })
+      .then((res) => res.text())
+      .then((data) => {
+        setGames([]);
+        var xml = new XMLParser().parseFromString(data);
+        // console.log(xml.children[9]);
+        setGames(xml.children);
       })
-        .then((res) => res.text())
-        .then((data) => {
-          
-          setGames([]);
-          var xml = new XMLParser().parseFromString(data);
-          setGames(xml.children);
-        })
-        .catch((err) => console.log);
-    
+      .catch((err) => console.log);
   };
 
   return (
     <div className="App">
-      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
       <button onClick={handleClick}>import BGG</button>
       <p>Total Games: {games.length}</p>
       <div className="grid">
         {games.map((game) => (
           <div key={game.attributes.objectid}>
             <div className="img-container">
-              <img src={game.children[2].value} alt=""  />
+              <img src={game.children[2].value} alt="" />
             </div>
-            <h4> { game.children[0].value.search("&#039;")? game.children[0].value.replace("&#039;", "'"):game.children[0].value }</h4>
+           
+            {game.children[4].attributes.own === "1" && (
+              <span className="tag">own</span>
+            )}
+            {game.children[4].attributes.prevowned === "1" && (
+              <span className="tag">prev-owned</span>
+            )}
+            {game.children[4].attributes.wanttoplay === "1" && (
+              <span className="tag">want to play</span>
+            )}
+            {game.children[4].attributes.want === "1" ||
+              game.children[4].attributes.wanttobuy === "1" ||
+              game.children[4].attributes.wishlist === "1" && (
+                <span className="tag">whishlist</span>
+              )}
+            {game.children[4].attributes.preordered === "1" && (
+              <span className="tag">preorder</span>
+            )}
+            {game.children[4].attributes.fortrade === "1" && (
+              <span className="tag">for trade</span>
+            )}
+            <h4>
+              {" "}
+              {game.children[0].value.search("&#039;")
+                ? game.children[0].value.replace("&#039;", "'")
+                : game.children[0].value}
+            </h4>
             <span>Plays: {game.children[5].value}</span>
           </div>
         ))}
